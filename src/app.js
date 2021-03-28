0c3c5a2c77bc1e5e4dc81100626b42d44eaecacb
Gaurav <itsgaurav01@gmail.com>
2021-03-29 02:30:55 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const getWeather = require('./utils/getWeather')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Gaurav Singh'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Gaurav Singh'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        description: 'This is the help page of the Weather App',
+        name: 'Gaurav Singh'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    
+    if(!req.query.address)
+    {
+        return res.send({
+            error: 'Address not provided'
+        })
+    }
+
+    getWeather(req.query.address, (error, {location, forecast} = {}) => {
+        if(error) {
+            return res.send({
+                error
+            })
+        }
+        else {
+            return res.send({
+                location,
+                forecast,
+                address: req.query.address
+            })
+        }
+    })
+})
+ 
+app.get('/products', (req, res) => {
+    
+    if(!req.query.search) {
+        return res.send({
+            error: 'Please provide a search query'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Gaurav Singh',
+        error: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Gaurav Singh',
+        error: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
---
+++ src/utils/getWeather.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const getWeather = (location, callback) => {
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(location) + '&appid=40e424c3d86a901d693a3c89eaf4a447'
+    request({url, json : true}, (error, {body} = {}) => {
+        if(error) {
+            callback('Unable connect to the Weather service.', undefined);
+        } 
+        else if(!body.coord) {
+            callback('Location not found. Please try again.', undefined);
+        }
+        else {
+            forecast = "The current temperature is " + Math.round((body.main.temp - 273.15) * 100) / 100 + " degrees. It feels like " + Math.round((body.main.feels_like - 273.15) * 100)/100 + " degrees."
+            callback(undefined, {
+                location: body.name + ', ' + body.sys.country,
+                forecast: forecast
+            });
+        }
+    })
+}
+
+module.exports = getWeather
