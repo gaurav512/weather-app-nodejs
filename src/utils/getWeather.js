45e9fee971742ec17925bee2a2bccdf6acb049ff
Gaurav <itsgaurav01@gmail.com>
2021-03-29 13:20:55 +0530

    Fixed timezone bug

--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -11,12 +11,12 @@ const getWeather = (location, callback) => {
             callback('Location not found. Please try again.', undefined);
         }
         else {
-            sunrise = moment(body.sys.sunrise*1000).local().format('HH:mm A')
-            sunset = moment(body.sys.sunset*1000).local().format('HH:mm A')
+            sunrise = moment(body.sys.sunrise*1000).utcOffset('+0530').format('hh:mm A')
+            sunset = moment(body.sys.sunset*1000).utcOffset('+0530').format('hh:mm A')
             
             forecast = `<br><p>Weather summary: ${body.weather[0].description}</p>`
             forecast += `<p>The current temperature is ${Math.round((body.main.temp - 273.15) * 100) / 100} degrees. It feels like ${Math.round((body.main.feels_like - 273.15) * 100)/100} degrees.</p>`
-            forecast += `<p>Sunrise: ${sunrise}&emsp;Sunset: ${sunset}</p>`
+            forecast += `<p>Sunrise: ${sunrise}&emsp;Sunset: ${sunset}&emsp;(+5:30 UTC)</p>`
             callback(undefined, {
                 location: body.name + ', ' + body.sys.country,
                 forecast: forecast
